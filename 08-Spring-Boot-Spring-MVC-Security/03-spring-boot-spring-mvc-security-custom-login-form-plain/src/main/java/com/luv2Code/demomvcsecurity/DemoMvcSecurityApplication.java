e17c352c5be1d3a411fc063ebe7b236fa91af57d
Mostafa Mahmoud <mostafamahmoud.cse@gmail.com>
2025-03-10 10:29:41 +0200

    Custom Login Form (Plain)

---
+++ 08-Spring-Boot-Spring-MVC-Security/03-spring-boot-spring-mvc-security-custom-login-form-plain/src/main/java/com/luv2Code/demomvcsecurity/DemoMvcSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.luv2Code.demomvcsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoMvcSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoMvcSecurityApplication.class, args);
+	}
+
+}
