00999306f3bbef74aba6f10aa2781bf6964a0081
Mostafa Mahmoud <mostafamahmoud.cse@gmail.com>
2025-03-17 10:36:09 +0200

    Adding JDBC Authentication (Plain Text)

---
+++ 08-Spring-Boot-Spring-MVC-Security/11-spring-boot-spring-mvc-security-jdbc-plain-text/src/test/java/com/luv2Code/demomvcsecurity/DemomvcsecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luv2Code.demomvcsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemomvcsecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
